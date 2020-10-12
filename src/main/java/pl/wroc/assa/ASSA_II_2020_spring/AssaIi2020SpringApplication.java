1c31a6ff29ae30ec85812c3d33896979be0413d4
scina <dudek.mail@gmail.com>
2020-10-12 08:44:11 +0200

    Initial commit

---
+++ src/main/java/pl/wroc/assa/ASSA_II_2020_spring/AssaIi2020SpringApplication.java
@@ -0,0 +1,13 @@
+package pl.wroc.assa.ASSA_II_2020_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssaIi2020SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssaIi2020SpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/wroc/assa/ASSA_II_2020_spring/AssaIi2020SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wroc.assa.ASSA_II_2020_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssaIi2020SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
