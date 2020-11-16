4e93d421fbaf0dc64e984c1ba638598b8ba3e1b5
Przemek Assa <dudek.assa@gmail.com>
2020-11-16 09:21:18 +0100

    Lekcja 16.11.2020 - przetwarzanie danych z formularza

--- src/main/java/pl/wroc/assa/ASSA_II_2020_spring/NameController.java
+++ src/main/java/pl/wroc/assa/ASSA_II_2020_spring/NameController.java
@@ -4,6 +4,9 @@ package pl.wroc.assa.ASSA_II_2020_spring;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class NameController {
@@ -16,10 +19,10 @@ public class NameController {
 
     @GetMapping("/")
     public String home(Model model) {
-        String name = "Przemyslaw";
-        model.addAttribute("nameSize", nameService.getNameSize(name));
-        model.addAttribute("isEvenLettersInName", nameService.isEvenLettersInName(name));
-        model.addAttribute("getName", new GetNameForm());
+//        String name = "Przemyslaw";
+//        model.addAttribute("nameSize", nameService.getNameSize(name));
+//        model.addAttribute("isEvenLettersInName", nameService.isEvenLettersInName(name));
+        model.addAttribute("getNameForm", new GetNameForm());
         return "index";
     }
 
@@ -33,5 +36,15 @@ public class NameController {
     }
 
     //tutaj ma byc Twój link
+
+    @PostMapping("/get_name")
+    public String getNameForm(@ModelAttribute("getNameForm") GetNameForm getNameForm,
+                              RedirectAttributes redirectAttributes) {
+        redirectAttributes.addFlashAttribute("name", getNameForm.getName());
+        redirectAttributes.addFlashAttribute("nameSize", nameService.getNameSize(getNameForm.getName()));
+        redirectAttributes.addFlashAttribute("isEvenLettersInName", nameService.isEvenLettersInName(getNameForm.getName()));
+
+        return "redirect:/";
+    }
 
 }
