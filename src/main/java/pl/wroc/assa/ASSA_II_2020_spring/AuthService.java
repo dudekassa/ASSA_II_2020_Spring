1ade21d12571272045221ca1064d030de61f1941
Przemek Assa <dudek.assa@gmail.com>
2020-11-30 08:24:43 +0100

    Lekcja 30.11.2020 - baza z loginem i hasłem

--- src/main/java/pl/wroc/assa/ASSA_II_2020_spring/AuthService.java
+++ src/main/java/pl/wroc/assa/ASSA_II_2020_spring/AuthService.java
@@ -2,10 +2,19 @@ package pl.wroc.assa.ASSA_II_2020_spring;
 
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Service
 public class AuthService {
 
     private final String PASSWORD = "tajne";
+
+    private final Map<String, String> userWithPassword = new HashMap<>();
+
+    public AuthService() {
+        userWithPassword.put("przemek", "tajne");
+    }
 
     public boolean tryLogin(String password) {
         return password.equals(PASSWORD);
