0163b20c4ebf8bbfafb91f9564f7639d03a765d3
Przemysław Dudek <dudek.mail@gmail.com>
2021-03-29 08:58:33 +0200

    feat: Lekcja 29.03.2021 - Spring Security.

---
+++ src/main/java/pl/wroc/assa/ASSA_II_2020_spring/SecurityConfiguration.java
@@ -0,0 +1,21 @@
+package pl.wroc.assa.ASSA_II_2020_spring;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                    .formLogin().permitAll()
+                .and()
+                    .logout().permitAll();
+    }
+}
