787d58d9cbb4132ecd5a178e310c022220bed581
Przemysław Dudek <dudek.mail@gmail.com>
2021-04-12 08:33:39 +0200

    TASK: Lekcja 12.04.2021 - Zadanie 2.

--- src/main/java/pl/wroc/assa/ASSA_II_2020_spring/HomeController.java
+++ src/main/java/pl/wroc/assa/ASSA_II_2020_spring/HomeController.java
@@ -1,38 +1,12 @@
 package pl.wroc.assa.ASSA_II_2020_spring;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import pl.wroc.assa.ASSA_II_2020_spring.authentication.AuthService;
-import pl.wroc.assa.ASSA_II_2020_spring.authentication.LoginForm;
-import pl.wroc.assa.ASSA_II_2020_spring.authentication.SessionService;
-import pl.wroc.assa.ASSA_II_2020_spring.authentication.UserService;
-import pl.wroc.assa.ASSA_II_2020_spring.nameapp.NameService;
-
 @Controller
 public class HomeController {
 
-    private final NameService nameService;
-    private final AuthService authService;
-    private final SessionService sessionService;
-    private final UserService userService;
-
-    public HomeController(NameService nameService, AuthService authService, SessionService sessionService, UserService userService) {
-        this.nameService = nameService;
-        this.authService = authService;
-        this.sessionService = sessionService;
-        this.userService = userService;
-    }
-
-
-
-
     @GetMapping("/")
-    public String home(Model model) {
-        model.addAttribute("loginForm", new LoginForm());
-        model.addAttribute("isLogin", sessionService.isLogin());
-        model.addAttribute("userName", sessionService.getUserName());
-        model.addAttribute("users", authService.getUsers());
+    public String home() {
         return "index";
     }
 
